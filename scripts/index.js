06b153fe09b440f5638941979fe787fb1c433701
Sebastian Alfaro <okrican@Mac.attlocal.net>
2025-05-27 16:04:02 -0400

    Complete Project 4 Final Stage: dynamic cards and profile modal

--- scripts/index.js
+++ scripts/index.js
@@ -31,7 +31,16 @@ const modal = document.querySelector(".modal");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".modal__close-button");
 
+const nameInput = document.querySelector('input[name="name"]');
+const jobInput = document.querySelector('input[name="description"]');
+
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__description');
+const profileFormElement = document.querySelector('.modal__form');
+
 function openModal() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   modal.classList.add("modal_opened");
 }
 
@@ -39,5 +48,35 @@ function closeModal() {
   modal.classList.remove("modal_opened");
 }
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); 
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closeModal(); 
+}
+
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
 editButton.addEventListener("click", openModal);
 closeButton.addEventListener("click", closeModal);
+
+const cardTemplate = document.querySelector('#card-template');
+const cardsList = document.querySelector('.cards__list');
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
+  
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+
+initialCards.forEach(function(cardData) {
+  const cardElement = getCardElement(cardData);
+  cardsList.append(cardElement);
+});
