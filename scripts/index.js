ac116f1fa61170f36495366af1319f33fcd0ca6f
Sebastian Alfaro <okrican@Mac.attlocal.net>
2025-08-05 21:08:57 -0400

    Final Sprint 6 project submission with validation fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,40 @@
 const initialCards = [
-  {
-    name: 'Yosemite Valley',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg'
-  },
-  {
-    name: 'Lake Louise',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
-  },
-  {
-    name: 'Bald Mountains',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg'
-  },
-  {
-    name: 'Latemar',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg'
-  },
-  {
-    name: 'Vanoise National Park',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg'
-  },
-  {
-    name: 'Lago di Braies',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg'
-  }
+  { name: 'Yosemite Valley', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg' },
+  { name: 'Lake Louise', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg' },
+  { name: 'Bald Mountains', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg' },
+  { name: 'Latemar', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg' },
+  { name: 'Vanoise National Park', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg' },
+  { name: 'Lago di Braies', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg' }
 ];
 
 function openModal(modal) {
   modal.classList.add("modal_opened");
+  document.addEventListener("keydown", handleEscClose);
 }
 
 function closeModal(modal) {
   modal.classList.remove("modal_opened");
+  document.removeEventListener("keydown", handleEscClose);
 }
 
-const profileModal = document.querySelector('.modal'); 
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const openModal = document.querySelector(".modal_opened");
+    if (openModal) closeModal(openModal);
+  }
+}
+
+document.querySelectorAll(".modal").forEach((modal) => {
+  modal.addEventListener("mousedown", (evt) => {
+    if (evt.target.classList.contains("modal")) {
+      closeModal(modal);
+    }
+  });
+});
+
+const profileModal = document.querySelector('.modal');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = profileModal.querySelector('.modal__close-button');
-
 const nameInput = document.querySelector('input[name="name"]');
 const jobInput = document.querySelector('input[name="description"]');
 const profileName = document.querySelector('.profile__title');
@@ -53,10 +51,10 @@ editButton.addEventListener("click", openProfileModal);
 closeButton.addEventListener("click", () => closeModal(profileModal));
 
 function handleProfileFormSubmit(evt) {
-  evt.preventDefault(); 
+  evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closeModal(profileModal); 
+  closeModal(profileModal);
 }
 
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
@@ -79,7 +77,7 @@ function handleAddCardFormSubmit(evt) {
   };
   const newCard = getCardElement(newCardData);
   cardsList.prepend(newCard);
-  addCardForm.reset(); 
+  addCardForm.reset();
   closeModal(addCardModal);
 }
 
@@ -104,7 +102,6 @@ const cardsList = document.querySelector('.cards__list');
 
 function getCardElement(data) {
   const cardElement = cardTemplate.content.cloneNode(true).querySelector('.card');
-
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
   const likeButton = cardElement.querySelector('.card__like-button');
@@ -132,4 +129,13 @@ function getCardElement(data) {
 initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
   cardsList.append(cardElement);
+});
+
+enableValidation({
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-button",
+  inactiveButtonClass: "modal__save-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible"
 });
